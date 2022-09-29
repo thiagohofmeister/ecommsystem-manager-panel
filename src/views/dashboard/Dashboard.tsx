33ed7258c6c8a195fb566e9700483845d26ab55b
Thiago Hofmeister <thiago.hofmeister@gmail.com>
2022-09-28 22:26:10 -0300

    Adding PageTitle component on Dashboard page

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -1,7 +1,13 @@
 import './Dashboard.scss'
 
+import PageTitle from '../../components/pageTitle/PageTitle'
+
 const Dashboard = () => {
-  return <div className="dashboard">Dashboard</div>
+  return (
+    <div className="dashboard">
+      <PageTitle title="Dashboard" />
+    </div>
+  )
 }
 
 export default Dashboard
